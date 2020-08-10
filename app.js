bc038e5d23dd1181d6171f9b479fca5113f3c3aa
Chris De Gour <cdegour@cdegour-ltm01.internal.salesforce.com>
2020-08-10 09:50:46 -0700

    hmmm

--- app.js
+++ app.js
@@ -36,10 +36,14 @@ const server = express()
 const dataHandler = function(messageSet, topic, partition){
     messageSet.forEach(function(m){
         //do stuff
-        console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
+        //console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
+        console.log("message found");
+        console.log(m.message);
+        console.log(m.message.value);
+        console.log(m.message.payload);
         pool.connect((err, client, done) => {
           //eventPayload shortcut
-          let eP = m.message.value.payload.after;
+          let eP = m.message.payload.after;
           if(err) throw err;
           client.query('INSERT INTO "public.account"(id,billingcountry,accountsource,billingpostalcode,billingcity,billingstate,description,billinglatitude,website,phone,fax,createddate,billingstreet,lastmodifieddate,external_id__c,name,billinglongitude,sfid,score__c) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19)',
           [billingcountry,eP.accountsource,eP.billingpostalcode,eP.billingcity,eP.billingstate,eP.description,eP.billinglatitude,eP.website,eP.phone,eP.fax,eP.createddate,eP.billingstreet,eP.lastmodifieddate,eP.external_id__c,eP.name,eP.billinglongitude,eP.sfid,eP.score__c], (err, res) => {
@@ -55,5 +59,5 @@ const dataHandler = function(messageSet, topic, partition){
 };
 return consumer.init().then(function(){
   console.log(`${process.env.KAFKA_PREFIX}${process.env.KAFKA_TOPIC}`)
-    return consumer.subscribe(`${process.env.KAFKA_PREFIX}${process.env.KAFKA_TOPIC}`, [0,1], dataHandler);
+    return consumer.subscribe(`${process.env.KAFKA_PREFIX}${process.env.KAFKA_TOPIC}`, dataHandler);
 })
