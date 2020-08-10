697bec2d1a0ac8cc83ac2d947045a07953e9f8b7
Chris De Gour <cdegour@cdegour-ltm01.internal.salesforce.com>
2020-08-10 09:19:19 -0700

    base ready

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
-const socketIO = require('socket.io');
 const path = require('path');
 const Kafka = require('no-kafka');
 const fs = require('fs');
+const {Pool, Client} = require('pg');
 
 fs.writeFileSync('./client.crt', process.env.KAFKA_CLIENT_CERT);
 fs.writeFileSync('./client.key', process.env.KAFKA_CLIENT_CERT_KEY);
@@ -16,6 +16,12 @@ const consumer = new Kafka.SimpleConsumer({
     },
 });
 
+const pool = new Pool();
+pool.on('error', (err, client) => {
+  console.error('Had an error on the PG idle client', err);
+  process.exit(-1);
+});
+
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
@@ -24,22 +30,24 @@ const server = express()
   .use('/', (req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
-function callSockets(io, message){
-  io.sockets.emit('update', message);
-}
-
 const dataHandler = function(messageSet, topic, partition){
     messageSet.forEach(function(m){
         //do stuff
         console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
-        callSockets(io, m.message.value.toString('utf8'));
+        pool.connect((err, client, done) => {
+          //eventPayload shortcut
+          let eP = m.message.value.payload.after;
+          if(err) throw err;
+          client.query('INSERT INTO "public.account"(id,billingcountry,accountsource,billingpostalcode,billingcity,billingstate,description,billinglatitude,website,phone,fax,createddate,billingstreet,lastmodifieddate,external_id__c,name,billinglongitude,sfid,score__c) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19)',
+          [billingcountry,eP.accountsource,eP.billingpostalcode,eP.billingcity,eP.billingstate,eP.description,eP.billinglatitude,eP.website,eP.phone,eP.fax,eP.createddate,eP.billingstreet,eP.lastmodifieddate,eP.external_id__c,eP.name,eP.billinglongitude,eP.sfid,eP.score__c], (err, res) => {
+            done();
+            if(err){
+              console.error(err);
+            }else{
+              console.log('Insert completed probably maybe');
+            }
+          });
+        });
     });
 };
 return consumer.init().then(function(){
